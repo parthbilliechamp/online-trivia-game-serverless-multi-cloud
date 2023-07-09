1b19d01f185d40cebf1c5a83afa2f905e2ed0839
Jainil Sevalia <jn498899@dal.ca>
2023-07-09 14:39:44 -0300

    Fetching firestore cerdentials from secrete manage

--- server/microservices/trivia_games/lambda_function.py
+++ server/microservices/trivia_games/lambda_function.py
@@ -2,15 +2,27 @@ import json
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+import boto3
 
-cred = credentials.Certificate('./Trivia_firestore_key.json')
-firebase_admin.initialize_app(cred, {
-    'projectId': 'trivia-392000',
-})
-
-db = firestore.client()
+def get_firebase_key():
+    secret_name = "firestore_db_key"
+    region_name = "us-east-1" 
+    session = boto3.session.Session()
+    client = session.client(service_name="secretsmanager", region_name=region_name)
+    secret_value = client.get_secret_value(SecretId=secret_name)["SecretString"]
+    firebase_key = json.loads(secret_value)
+    return firebase_key
 
 def handler(event, context):
+    firebase_key = get_firebase_key()
+
+    cred = credentials.Certificate(firebase_key)
+    firebase_admin.initialize_app(cred, {
+        'projectId': 'trivia-392000',
+    })
+
+    db = firestore.client()
+
     category = event.get('queryStringParameters', {}).get('category')
     difficulty_level = event.get('queryStringParameters', {}).get('difficultyLevel')
     time_frame = event.get('queryStringParameters', {}).get('timeFrame')
