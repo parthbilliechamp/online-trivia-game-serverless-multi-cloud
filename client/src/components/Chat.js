b2f16f477bf724b7a53b4cf44c7d368f88a9598a
Raj Nileshkumar Patel <rj506049@dal.ca>
2023-08-05 11:27:17 -0300

    chatbot with Kommunicate

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -1,25 +1,31 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 
-class KommunicateChat extends Component {
-    constructor (props) {
-        super (props);
-    }
+const KommunicateChat = () => {
+  useEffect(() => {
+    const kommunicateSettings = {
+      appId: '1a0f260915cf48815a2fffa7815edd395',
+      popupWidget: true,
+      automaticChatOpenOnNavigation: true,
+    };
 
-    componentDidMount (){
-        (function(d, m){
-            var kommunicateSettings = {"appId":"aii-igbc2","popupWidget":true,"automaticChatOpenOnNavigation":true};
-            var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
-            s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
-            var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
-            window.kommunicate = m; m._globals = kommunicateSettings;
-          })(document, window.kommunicate || {});
-        }
-    render () {
-    return (
-        <div></div>
-    )
+    // Check if Kommunicate is already loaded
+    if (!window.kommunicate) {
+      (function (d, m) {
+        var s = document.createElement('script');
+        s.type = 'text/javascript';
+        s.async = true;
+        s.src = 'https://widget.kommunicate.io/v2/kommunicate.app';
+        var h = document.getElementsByTagName('head')[0];
+        h.appendChild(s);
+        window.kommunicate = m;
+        m._globals = kommunicateSettings;
+      })(document, window.kommunicate || {});
+    } else {
+      window.kommunicate._globals = kommunicateSettings;
     }
-    
-}
+  }, []);
+
+  return null; // Return null since this component doesn't render anything
+};
 
 export default KommunicateChat;
