e422f062d87479ddea8719e48f9cf32c8bfa4fbb
Raj Nileshkumar Patel <rj506049@dal.ca>
2023-08-04 22:38:42 -0300

    Chatbot

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -31,6 +31,7 @@ import CreateTeamPage from "../pages/CreateTeamPage";
 import InvitedUserPage from "../pages/InvitedUserPage";
 import LobbyPage from "../pages/LobbyPage";
 import Logout from "../components/Logout";
+import KommunicateChat from "../components/Chat";
 import TeamMemberList from "../components/TeamMemberList";
 
 const AppRoutes = () => {
@@ -54,6 +55,7 @@ const AppRoutes = () => {
         <Route path="/logout" element={<Logout />} />
         <Route path="/addquestion" element={<QuestionForm />} />
         <Route path="/admin" element={<HomePage />} />
+        <Route path="/aihelp" element={<KommunicateChat />} />
 
         <Route path="/getquestion" element={<QuestionList />} />
         <Route
