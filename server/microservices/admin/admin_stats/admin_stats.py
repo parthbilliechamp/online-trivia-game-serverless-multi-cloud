c1f75c33bddd5406a36eb1e68543360fbf19ccb1
dkothari <dh801135@dal.ca>
2023-08-05 16:37:09 -0300

    Yaml File

---
+++ server/microservices/admin/admin_stats/admin_stats.py
@@ -0,0 +1,44 @@
+import json
+import boto3
+import time
+
+REGION = 'us-east-1'
+ATHENA_DB = 'trivia_quiz_etl_db'
+ATHENA_VIEW = 'admin_stats'  #view for admin stats
+S3_OUTPUT_PATH = 's3://glue-etl-target-trivia-game/adminresults/'
+
+def lambda_handler(event, context):
+    athena_client = boto3.client('athena')
+    
+    # Modified query to get all admin_updates
+    queryStart = athena_client.start_query_execution(
+        QueryString = 'SELECT * FROM ' + ATHENA_VIEW,
+        QueryExecutionContext = {
+            'Database': ATHENA_DB
+        }, 
+        ResultConfiguration = { 'OutputLocation': S3_OUTPUT_PATH}
+    )
+    
+    queryExecution = athena_client.get_query_execution(QueryExecutionId=queryStart['QueryExecutionId'])
+    
+    while queryExecution['QueryExecution']['Status']['State'] not in ['SUCCEEDED', 'FAILED', 'CANCELLED']:
+        queryExecution = athena_client.get_query_execution(QueryExecutionId=queryStart['QueryExecutionId'])
+        time.sleep(2)
+    
+    result_response = athena_client.get_query_results(QueryExecutionId=queryStart['QueryExecutionId'])
+    result_data = result_response['ResultSet']['Rows'][1:]
+        
+    # Parse the query results
+    admin_stats = {}
+    for row in result_data:
+        column_name = row['Data'][0]['VarCharValue']
+        column_value = row['Data'][1]['VarCharValue']
+        admin_stats[column_name] = column_value
+
+    print("Admin Stats:")
+    print(admin_stats)
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps({'admin_stats': admin_stats})
+    }
